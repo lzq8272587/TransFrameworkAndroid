296ca731c3ad5ec152b9cf2973a8c5e8a8e1cc1c
ymjiang <ymjiang820@gmail.com>
2015-11-15 15:49:52 +0800

    Update HurlStack.java
    增加了分块传输和错误恢复模块

--- volley/src/main/java/com/android/volley/toolbox/HurlStack.java
+++ volley/src/main/java/com/android/volley/toolbox/HurlStack.java
@@ -34,6 +34,7 @@ import org.apache.http.message.BasicStatusLine;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.RandomAccessFile;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.HashMap;
@@ -193,6 +194,69 @@ public class HurlStack implements HttpStack {
         }
         URL parsedUrl = new URL(url);
         HttpURLConnection connection = openConnection(parsedUrl, request);
+        //---------------------------------modified by JYM---------------------------------//
+        //增加分块传输和错误恢复模块
+        String linkString = "http://107.161.26.199/temp";
+        int blockSize = 512;
+        String sPath = "e://temp";
+        int nStartPos = 0;
+        int timeout = 15000;
+        int NUMLIMIT_FAIL=5;
+        long nEndPos = 30000000;
+        byte[] b = new byte[blockSize];
+
+        try {
+            connection.setConnectTimeout(timeout);
+            connection.setReadTimeout(timeout);
+            String sProperty = "bytes="  + nStartPos +  "-" +nEndPos;   //sProperty设置的是开始下载的位置，即断点。若nStartPos为0则完整下载整个文件
+            connection.setRequestProperty("RANGE" , sProperty);
+            //当然也可以这么写：httpConnection.setRequestProperty("RANGE","bytes=50-20070");
+            InputStream input = connection.getInputStream();
+            RandomAccessFile oSavedFile = new RandomAccessFile(sPath, "rw");
+            //读取网络文件,写入指定的文件中
+            int nRead,failedCnt=1;
+            System.out.println("Connect successfully\nBegin downloading...");
+            while (nStartPos < nEndPos&&failedCnt<=NUMLIMIT_FAIL) {
+                try {
+                    nRead = input.read(b, 0, blockSize);
+                    if(nRead>0){
+                        oSavedFile.write(b, 0, nRead);
+                        nStartPos += nRead;
+                    }
+                    else {
+                        break;
+                    }
+                    System.out.println(nStartPos*100/nEndPos+"%");
+                } catch (Exception e) {
+                    System.out.println("Network error, try to reconnect ("+failedCnt+")");
+                    failedCnt++;
+
+                    connection.disconnect();
+                    connection = (HttpURLConnection) parsedUrl.openConnection();
+                    connection.setConnectTimeout(timeout);
+                    connection.setReadTimeout(timeout);
+                    sProperty = "bytes="  + nStartPos +  "-" +nEndPos;   //sProperty设置的是开始下载的位置，即断点。若nStartPos为0则完整下载整个文件
+                    connection.setRequestProperty("RANGE" , sProperty);
+                    //当然也可以这么写：httpConnection.setRequestProperty("RANGE","bytes=50-20070");
+                    input = connection.getInputStream();
+                }
+            }
+            if(failedCnt>NUMLIMIT_FAIL){
+                System.out.println("Download failed.");
+            }
+            else {
+                System.out.println("Download successfully!");
+            }
+            connection.disconnect();
+            oSavedFile.close();
+        } catch (Exception e1) {
+            e1.printStackTrace();
+            //System.out.println("Connection failed.");
+        }
+        System.out.println("Ends.");
+        //---------------------------------modified by JYM(ends)---------------------------------//
+
+
         for (String headerName : map.keySet()) {
             connection.addRequestProperty(headerName, map.get(headerName));
         }
